95b7ce04a27e304a5913e058d0d56d3868b47625
Alan Smillie <alan.smillie@gmail.com>
2021-01-20 11:25:24 -0500

    Add health check endpoint

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const port = parseInt(process.env.SERVER_PORT, 10) || 8100;
 
 app.use(express.static('./www'));
 
+// Health Check Endpoint
+app.get('/health', (req, res) => {
+    res.status(200).send('Server Running');
+});
+
+// Redirect all other requests to index.html
 app.get('/*', (req, res) => {
     res.sendFile('index.html', {root: 'www/'});
 });
